6b3229053621eeefc3092ad8f46c4877efc43890
kibnm821 <kibnm821@naver.com>
2019-08-12 11:38:51 +0900

    0812

--- SystemlnScanner.java
+++ SystemlnScanner.java
@@ -4,6 +4,7 @@ public class SystemlnScanner{
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         System.out.printf("\nEnter the first character : ");
+
         int ch1 = scanner.nextInt();
         System.out.printf("After conversion : " + ch1);
     }
