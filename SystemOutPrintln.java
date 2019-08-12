cb93f49424a891622f73f0624bfb09ea7f9c6d5c
kibnm821 <kibnm821@naver.com>
2019-08-12 11:34:49 +0900

    0812

---
+++ SystemOutPrintln.java
@@ -0,0 +1,5 @@
+public class SystemOutPrintln{
+    public static void main(String args[]){
+        System.out.println("she said \"Hello\" to me.");
+    }
+}
---
+++ SystemlnScanner.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class SystemlnScanner{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("\nEnter the first character : ");
+        int ch1 = scanner.nextInt();
+        System.out.printf("After conversion : " + ch1);
+    }
+}
